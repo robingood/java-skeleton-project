a73a2965ec927fd90f0d8c9f86a618837ea688fa
Remus Dugheanu <remuscristian.dugheanu@zf.com>
2023-03-26 17:28:34 +0200

    simple controller

---
+++ src/main/java/org/course/skeleton/controller/HelloController.java
@@ -0,0 +1,18 @@
+package org.course.skeleton.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(value = "")
+public class HelloController {
+
+    @GetMapping(value = "/hello/{name}")
+    @ResponseBody
+    public String helloByName(@PathVariable String name) {
+        return "Hello " + name;
+    }
+}
