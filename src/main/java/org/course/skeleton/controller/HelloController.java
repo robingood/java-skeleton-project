c108bb7a76d5948e100a8d8d1fc96287e54307e7
Remus Dugheanu <remuscristian.dugheanu@zf.com>
2023-05-02 16:36:52 +0200

    data structures

--- src/main/java/org/course/skeleton/controller/HelloController.java
+++ src/main/java/org/course/skeleton/controller/HelloController.java
@@ -1,5 +1,6 @@
 package org.course.skeleton.controller;
 
+import org.course.skeleton.dto.BookType;
 import org.course.skeleton.model.Book;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,22 +8,72 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.*;
+
 @Controller
 @RequestMapping(value = "")
 public class HelloController {
 
-    private int myNumber = 0;
+    private final int myNumber = 0;
+
+    @GetMapping(value = "/book-by-key/{key}")
+    @ResponseBody
+    public String getBookByName(@PathVariable String key) {
+        Map<String, Book> bookMap = new HashMap<>();
+        bookMap.put("book1", Book.builder()
+                .name("La tiganci")
+                .author("M. Eliade")
+                .type(BookType.COMEDY)
+                .pages(20)
+                .build());
+
+        bookMap.put("book2", Book.builder()
+                .name("La scaldat")
+                .author("I. Creanga")
+                .type(BookType.COMEDY)
+                .pages(20)
+                .build());
+
+        Book selectedBook = bookMap.getOrDefault(key, Book.builder().build());
+        return selectedBook.toString();
+    }
 
     @GetMapping(value = "/hello/{name}")
     @ResponseBody
     public String helloByName(@PathVariable String name) {
-        String name2 = name.concat("-test");
+
+        Set<Book> bookSet = new TreeSet<>();
+        bookSet.add(Book.builder()
+                .name("La tiganci")
+                .author("M. Eliade")
+                .type(BookType.COMEDY)
+                .pages(20)
+                .build());
+
+        bookSet.add(Book.builder()
+                .name("La scaldat")
+                .type(BookType.COMEDY)
+                .pages(20)
+                .build());
+
+        List<Book> bookList = new ArrayList<>();
+        bookList.add(Book.builder()
+                .name("La tiganci")
+                .type(BookType.COMEDY)
+                .pages(20)
+                .build());
+        return bookList.get(0).getName();
+
+        /*
+        Book[] myBooks = new Book[]{new Book("La scaldat", 20)};
+        return myBooks[0].getName();*/
+
+        /*
 
         Book book = new Book("La scaldat", 20);
         String capitalName = book.getName().toUpperCase();
 
-
-        myNumber = myNumber + 1;
+        myNumber = myNumber + 1;*7
 
         return "User " + name + " has read : " + book.getDescription();
 /*
--- src/main/java/org/course/skeleton/model/Book.java
+++ src/main/java/org/course/skeleton/model/Book.java
@@ -1,21 +1,25 @@
 package org.course.skeleton.model;
 
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import org.course.skeleton.dto.BookType;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Book implements IBook, Serializable {
+@EqualsAndHashCode
+@Builder
+public class Book implements IBook, Serializable, Comparable<Book> {
 
     private final String name;
 
+    private final String author;
     private final int pages;
 
-    private final List<Page> pageList = new ArrayList<>();
+    private final BookType type;
 
-    public Book(String name, int pages) {
-        this.name = name;
-        this.pages = pages;
-    }
+    private final List<Page> pageList = new ArrayList<>();
 
     public String getName() {
         return this.name;
@@ -32,6 +36,16 @@ public class Book implements IBook, Serializable {
     @Override
     public int getNumberOfPages() {
         return this.pages;
+    }
+
+
+    @Override
+    public int compareTo(Book o) {
+        return this.name.compareTo(o.getName());
+    }
+
+    public String toString() {
+        return "Book(name=" + this.getName() + ", author=" + this.author + ", pages=" + this.getPages() + ", type=" + this.type + ", pageList=" + this.pageList + ")";
     }
 
     private static class Page {
